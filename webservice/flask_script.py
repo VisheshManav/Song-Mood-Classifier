254905d4daada12b160c4f059619f30ac7d113b3
Vishesh Manav <xvishesh77@gmail.com>
2023-11-08 22:23:21 +0530

    created a flask service and request script

---
+++ webservice/flask_script.py
@@ -0,0 +1,20 @@
+import pickle
+from flask import Flask
+from flask import request, jsonify
+
+with open('model.pkl', 'rb') as model_file, open('features.pkl', 'rb') as feature_file:
+  model = pickle.load(model_file)
+  features = pickle.load(feature_file)
+
+app = Flask('song-mood-predictor')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  song_features = request.get_json()
+  X_features = [[song_features[k] for k in features]]
+  pred = model.predict(X_features)[0]
+  moods = ['sad', 'happy', 'energetic', 'calm']
+  return jsonify({'mood': moods[pred]})
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ webservice/request.py
@@ -0,0 +1,18 @@
+import requests
+
+song_features = {'Unnamed: 0': 119145.0,
+ 'duration (ms)': 240400.0,
+ 'danceability': 0.715,
+ 'energy': 0.653,
+ 'loudness': -7.031,
+ 'speechiness': 0.0403,
+ 'acousticness': 0.211,
+ 'instrumentalness': 0.0,
+ 'liveness': 0.148,
+ 'valence': 0.586,
+ 'tempo': 129.982,
+ 'spec_rate': 1.6763727121464226e-07
+}
+
+response = requests.post('http://localhost:9696/predict', json=song_features)
+print(response.json())
