d4240029926531ce3a68862a0ea45cca3f0fcd5d
Vishesh Manav <xvishesh77@gmail.com>
2023-11-29 14:31:30 +0530

    refactor & checking

--- webservice/flask_script.py
+++ webservice/flask_script.py
@@ -11,14 +11,25 @@ app = Flask('song-mood-predictor')
 @app.route('/predict', methods=['POST'])
 def predict():
   song_features = request.get_json()
+
+  has_features = all([k in song_features for k in features])
+  if not has_features:
+    return jsonify({'Error': 'Missing features\nFeatures needed are: '+features}), 400
+  
   X_features = [[song_features[k] for k in features]]
-  pred = model.predict(X_features)[0]
   moods = ['sad', 'happy', 'energetic', 'calm']
-  res = {moods[i]: {
-            'probability': float(prob),
-            'is_predicted': bool(pred == i) 
-          } 
-         for i, prob in enumerate(model.predict_proba(X_features)[0])}
+  
+  pred = model.predict(X_features)[0]
+  prob = model.predict_proba(X_features)[0]
+
+  mood_prob = zip(moods, prob)
+  sorted_mood_prob = sorted(mood_prob, key=lambda x: x[1])
+
+  res = {}
+  for i, m_prob in enumerate(sorted_mood_prob):
+    res[moods[i]] = {'probability': float(m_prob[1]), 
+                     'is_predicted': bool(pred == i)}
+    
   return jsonify(res)
 
 if __name__ == '__main__':
